7f57dfbd470d0768a9b36dd48449f6fd6017dd1d
Leandro Deveikis <leandro.deveikis@gmail.com>
2020-10-09 22:15:08 -0300

    Base spring-boot app - Hello world

---
+++ src/main/java/com/example/minesweeper/controller/MinesweeperController.java
@@ -0,0 +1,25 @@
+package com.example.minesweeper.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MinesweeperController {
+
+    @Value("${projectVersion}")
+    private String projectVersion;
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/version")
+    @ResponseBody
+    public String version() {
+        return "version: " + projectVersion;
+    }
+}
